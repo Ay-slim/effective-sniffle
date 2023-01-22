9c33ef50424f6197f15584c346611e08fced1625
Ayo <ayooluwaadedipe@gmail.com>
2023-01-22 17:50:11 +0000

    Bg colors

--- browser-extension/src/index.js
+++ browser-extension/src/index.js
@@ -39,6 +39,13 @@ function init() {
 		form.style.display = 'none';
 		clearBtn.style.display = 'block';
 	}
+
+  chrome.runtime.sendMessage({
+    action: 'updateIcon',
+      value: {
+        color: 'green',
+      },
+  });
 };
 
 function reset(e) {
@@ -75,9 +82,9 @@ async function displayCarbonUsage(apiKey, region) {
 				},
 			})
 			// .then((response) => {
-			// 	let CO2 = Math.floor(response.data.data.carbonIntensity);
+      let CO2 = Math.floor(response.data.data.carbonIntensity);
 
-			// 	//calculateColor(CO2);
+      calculateColor(CO2);
 
 			// 	loading.style.display = 'none';
 			// 	form.style.display = 'none';
@@ -89,19 +96,36 @@ async function displayCarbonUsage(apiKey, region) {
 			// 		'% (percentage of fossil fuels used to generate electricity)';
 			// 	results.style.display = 'block';
 			// });
-      loading.style.display = 'none';
-      form.style.display = 'none';
-      myregion.textContent = region;
-      usage.textContent =
-        Math.round(response.data.data.carbonIntensity) + ' grams (grams C02 emitted per kilowatt hour)';
-      fossilfuel.textContent =
-        response.data.data.fossilFuelPercentage.toFixed(2) +
-        '% (percentage of fossil fuels used to generate electricity)';
-      results.style.display = 'block';
+    loading.style.display = 'none';
+    form.style.display = 'none';
+    myregion.textContent = region;
+    usage.textContent =
+      Math.round(response.data.data.carbonIntensity) + ' grams (grams C02 emitted per kilowatt hour)';
+    fossilfuel.textContent =
+      response.data.data.fossilFuelPercentage.toFixed(2) +
+      '% (percentage of fossil fuels used to generate electricity)';
+    results.style.display = 'block';
 	} catch (error) {
 		console.log(error);
 		loading.style.display = 'none';
 		results.style.display = 'none';
 		errors.textContent = 'Sorry, we have no data for the region you have requested.';
 	}
+}
+
+function calculateColor(value) {
+	let co2Scale = [0, 150, 600, 750, 800];
+	let colors = ['#2AA364', '#F5EB4D', '#9E4229', '#381D02', '#381D02'];
+
+	let closestNum = co2Scale.sort((a, b) => {
+		return Math.abs(a - value) - Math.abs(b - value);
+	})[0];
+	console.log(value + ' is closest to ' + closestNum);
+	let num = (element) => element > closestNum;
+	let scaleIndex = co2Scale.findIndex(num);
+
+	let closestColor = colors[scaleIndex];
+	console.log(scaleIndex, closestColor);
+
+	chrome.runtime.sendMessage({ action: 'updateIcon', value: { color: closestColor } });
 }
