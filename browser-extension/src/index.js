cdb2dd849de2df88bc7aa7dae7132cca4f07d6af
Ayo <ayooluwaadedipe@gmail.com>
2023-01-22 10:55:20 +0000

    Set up scripting logic

--- browser-extension/src/index.js
+++ browser-extension/src/index.js
@@ -1,17 +1,48 @@
 //1
 // form fields
+const form = document.querySelector('.form-data');
+const region = document.querySelector('.region-name');
+const apiKey = document.querySelector('.api-key');
 // results divs
+const errors = document.querySelector('.errors');
+const loading = document.querySelector('.loading');
+const results = document.querySelector('.result-container');
+const usage = document.querySelector('.carbon-usage');
+const fossilfuel = document.querySelector('.fossil-fuel');
+const myregion = document.querySelector('.my-region');
+const clearBtn = document.querySelector('.clear-btn');
 
-//6
-//call the API
+form.addEventListener('submit', (e) => handleSubmit(e));
+clearBtn.addEventListener('click', (e) => reset(e));
+init();
 
-//5
-//set up user's api key and region
+function init() {
+	//if anything is in localStorage, pick it up
+	const storedApiKey = localStorage.getItem('apiKey');
+	const storedRegion = localStorage.getItem('regionName');
 
-//4
-// handle form submission
+	//set icon to be generic green
+	//todo
 
-//3 initial checks
+	if (storedApiKey === null || storedRegion === null) {
+		//if we don't have the keys, show the form
+		form.style.display = 'block';
+		results.style.display = 'none';
+		loading.style.display = 'none';
+		clearBtn.style.display = 'none';
+		errors.textContent = '';
+	} else {
+    //if we have saved keys/regions in localStorage, show results when they load
+    displayCarbonUsage(storedApiKey, storedRegion);
+		results.style.display = 'none';
+		form.style.display = 'none';
+		clearBtn.style.display = 'block';
+	}
+};
 
-//2
-// set listeners and start app
+function reset(e) {
+	e.preventDefault();
+	//clear local storage for region only
+	localStorage.removeItem('regionName');
+	init();
+}
